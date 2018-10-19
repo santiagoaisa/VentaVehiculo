06c9471989d1a865fc2972a29bc6ac69e34bddbc
Sixto Santiago Aisa Incapuño <santiagoaisa@gmail.com>
2018-10-19 14:27:17 -0500

    modificacion CLiente

--- js/formularioCliente.js
+++ js/formularioCliente.js
@@ -34,22 +34,40 @@ function eliminarCliente(cdni){
 
 function modificarCliente(cdni){
     
-    for (const cliente of baseCliente) {
-        
+    for (const cliente of baseCliente) {        
         if(cliente.obtenerDni()==cdni){
             document.getElementById("nombrecliente").value=cliente.obtenerNombre();
-            document.getElementById("dnicliente").value=cliente.obtenerDni();
+            document.getElementById("dnicliente").value=cliente.obtenerDni();            
+            document.getElementById("dnicliente").disabled = true;
+            break;
+        }
+    }    
+}
+
+function actualizarCliente(){
+    let nombreCliente=document.getElementById("nombrecliente").value;
+    let dniCliente=document.getElementById("dnicliente").value; 
+   
+    for (const cliente of baseCliente) {        
+        if(cliente.obtenerDni()==dniCliente){
+            cliente.cambiarNombre(nombreCliente);
             break;
         }
     }
 
+    document.getElementById("tablaCliente").innerHTML="<tr><th>Nombre</th><th>DNI</th><th>Acción</th></tr> ";    
 
-    
+   
+    for (const cliente of baseCliente) {
+        mostrarClienteRegistrado(cliente);       
+    }
+
+    document.getElementById("dnicliente").disabled = false;
+
 }
 
 function mostrarClienteRegistrado(cliente){
     document.getElementById("tablaCliente").innerHTML+=`<tbody><td>${cliente.obtenerNombre()}</td> <td>${cliente.obtenerDni()}</td> <td> <button onclick="modificarCliente('${cliente.obtenerDni()}')" >Modificar</button>  <button onclick="eliminarCliente('${cliente.obtenerDni()}')" >Eliminar</button>  </td> </tbody> `;
-
 }
 
 function validarCliente(cdni){   
--- js/prueba.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-function Usuario(nombre){
-    this.nombre=nombre;
-}
