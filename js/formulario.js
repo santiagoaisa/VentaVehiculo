9623bbdd34bd356b276e0fcce4b55cedaffd9406
Sixto Santiago Aisa Incapuño <santiagoaisa@gmail.com>
2018-10-19 12:10:11 -0500

    Update js/formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -1,28 +1,26 @@
 "use strict";
 
-var listaClientes=[];
+var baseCliente=[];
 
 function registrarCliente(){
     let nombreCliente=document.getElementById("nombrecliente").value;
     let dniCliente=document.getElementById("dnicliente").value; 
 
-
     if(validarCliente(dniCliente)){
 
-            alert("DNI Duplicado");
+        alert("DNI Duplicado");
 
-        return ;
+        //si esta duplicado ya no continua
+         return ;
     }
-
 
     let cliente=new Cliente();
     cliente.cambiarNombre(nombreCliente);
     cliente.cambiarDni(dniCliente);
 
+    baseCliente.push(cliente);
 
-
-    mostrarClienteRegistrado(cliente);
-    
+    mostrarClienteRegistrado(cliente);    
 }
 
 function eliminarCliente(){
@@ -30,15 +28,15 @@ function eliminarCliente(){
 }
 
 function mostrarClienteRegistrado(cliente){
-
     document.getElementById("tablaCliente").innerHTML+=`<tbody><td>${cliente.obtenerNombre()}</td> <td>${cliente.obtenerDni()}</td> <td><a href="eliminarCliente()" >Eliminar</a> </td> </tbody> `;
 
 }
 
-function validarCliente(cdni){
+function validarCliente(cdni){   
 
-    let resultado=listaClientes.filter( dni => dni===cdni  );
+    let resultado=baseCliente.filter( cliente => cliente.obtenerDni()==cdni  );    
     
+    console.log('resultado '+resultado);
     return resultado.length>0;
 
 }
